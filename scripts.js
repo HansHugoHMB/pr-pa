7e7d180633efa1b14fec72d3f7441ad4e296ac05
HansHugoHMB <mbayahans@gmail.com>
2025-03-31 22:44:06 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-var encodedPath = "Y29udGVudC5odG1s";
-
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-            document.getElementById('contenu-dynamique').innerHTML = xhr.responseText;
-        } else {
-            console.error('Erreur lors du chargement du contenu:', xhr.status);
-            document.getElementById('contenu-dynamique').innerHTML = 'Erreur lors du chargement.';
-        }
-    }
-};
-
-// Décoder le chemin avant de l'utiliser
-xhr.open('GET', atob(encodedPath), true);
-xhr.send();
+var xhr = new XMLHttpRequest();  
+xhr.onreadystatechange = function() {  
+    if (xhr.readyState === 4) {  
+        if (xhr.status === 200) {  
+            document.getElementById('contenu-dynamique').innerHTML = xhr.responseText;  
+        } else {  
+            console.error('Erreur lors du chargement de content.html:', xhr.status);  
+            document.getElementById('contenu-dynamique').innerHTML = 'Erreur lors du chargement du contenu.';  
+        }  
+    }  
+};  
+xhr.open('GET', 'content.html', true);  
+xhr.send(); 
