2237e9c59b996af5aea2bde6d8dc0f60e81641a4
HansHugoHMB <mbayahans@gmail.com>
2025-03-31 23:01:43 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-var xhr = new XMLHttpRequest();  
-xhr.onreadystatechange = function() {  
-    if (xhr.readyState === 4) {  
-        if (xhr.status === 200) {  
-            document.getElementById('contenu-dynamique').innerHTML = xhr.responseText;  
-        } else {  
-            console.error('Erreur lors du chargement de content.html:', xhr.status);  
-            document.getElementById('contenu-dynamique').innerHTML = 'Erreur lors du chargement du contenu.';  
-        }  
-    }  
-};  
-xhr.open('GET', 'content.html', true);  
-xhr.send(); 
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            document.getElementById('contenu-dynamique').innerHTML = xhr.responseText;
+        } else {
+            console.error('Erreur lors du chargement de content.html:', xhr.status);
+            document.getElementById('contenu-dynamique').innerHTML = 'Erreur lors du chargement du contenu.';
+        }
+    }
+};
+xhr.open('GET', 'content.html', true);
+xhr.send();
